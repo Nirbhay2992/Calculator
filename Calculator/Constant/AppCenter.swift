d47ce0cc86687c07bf279f96442c831c32c294c2
Nirbhay Singh <nirbhay2992@gmail.com>
2024-03-09 20:48:20 +0530

    added scheme

---
+++ Calculator/Constant/AppCenter.swift
@@ -0,0 +1,10 @@
+//
+//  AppCenter.swift
+//  Calculator
+//
+//  Created by Nirbhay Singh on 09/03/24.
+//
+
+import Foundation
+
+let AppCenterKey = "723f98b5-e903-4a71-9ba7-71d21f459b08"
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -6,11 +6,20 @@
 //
 
 import UIKit
+import AppCenterCrashes
+import AppCenter
+import AppCenterAnalytics
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        AppCenter.configure(withAppSecret: AppCenterKey)
+        if AppCenter.isConfigured {
+            AppCenter.startService(Analytics.self)
+            AppCenter.startService(Crashes.self)
+        }
+        
     }
 
 
